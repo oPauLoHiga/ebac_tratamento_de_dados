903c523371bc9c0ee5e110ce6112042bc1231255
Paulo Higa <paulojhiga@gmail.com>
2025-10-16 17:34:29 -0300

    Update tratar_outliers.py

--- tratar_outliers.py
+++ tratar_outliers.py
@@ -18,3 +18,26 @@ print("Outliers pelo Z-score: \n", outliers_z)
 df_zscore = df[(stats.zscore(df['idade']) < 3)]
 
 # Identificar outliers com IQR
+Q1 = df['idade'].quantile(0.25)
+Q3 = df['idade'].quantile(0.75)
+IQR = Q3 - Q1
+
+limite_baixo = Q1 - 1.5 * IQR
+limite_alto = Q3 + 1.5 * IQR
+
+print("Limite IQR: \n",limite_baixo ,limite_alto)
+
+outliers_iqr = df[(df['idade'] < limite_baixo) | (df['idade'] > limite_alto)]
+print("Outliers IQR: \n",outliers_iqr)
+
+# Filtrar outliers com IQR
+df_iqr = df[(df['idade'] >= limite_baixo) | (df['idade'] <= limite_alto)]
+
+# Filtrar endereços invalidos
+df['endereco'] = df['endereco'].apply(lambda x: 'Endereço inválido' if len(x.split('\n')) < 3 else x)
+
+# Tratar campos de texto
+df['nome'] = df['nome'].apply(lambda x: 'Nome invalido' if isinstance(x,str) and len(x) > 50 else x)
+print('Qtd registros com nomes grandes:', (df['nome'] == 'Nome invalido').sum)
+
+print('Dados com Outliers tratados:\n', df)
