3a1019cc058a8d9ef5c2c4922d4b2e371d4a8dc0
Paulo Higa <paulojhiga@gmail.com>
2025-10-23 15:08:02 -0300

    Create inconsistencias.py

---
+++ inconsistencias.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
+df = pd.read_csv('clientes_remove_outliers.csv')
+
+print(df.head())
