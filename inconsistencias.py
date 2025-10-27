3962694355b36809fc2cfc468987ebe64e46f9b5
Paulo Higa <paulojhiga@gmail.com>
2025-10-27 17:55:30 -0300

    Update inconsistencias.py

--- inconsistencias.py
+++ inconsistencias.py
@@ -21,3 +21,29 @@ df['idade_ajustada'] -= ((data_atual.month <= df['idade_ajustada'].dt.month) & (
 df.loc[df['idade_ajustada'] > 100, 'idade_ajustada'] = np.nan
 
 # Corrigir campo com multiplas informações
+
+df['endereco_curto'] = df['endereco'].apply(lambda x: x.split('\n')[0].strip())
+df['bairro'] = df['endereco'].apply(lambda x: x.split('\n')[1].strip() if len(x.split('\n')) > 1 else 'Desconhecido')
+df['estado_sigla'] = df['endereco'].apply(lambda x: x.split(' / ')[-1].strip() if len(x.split('\n')) > 1 else 'Desconhecido')
+
+# Verificando a formatação do endereço
+df['endereco_curto'] = df['endereco_curto'].apply(lambda x: 'Endereço invalido' if len(x) > 50 or len(x) < 5 else x)
+
+# Corrigir dados erroneos
+df['cpf'] = df['cpf'].apply(lambda x: x if len(x) == 14 else 'CPF invalido')
+
+estados_br = [
+    'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA','MT', 'MS', 'MG', 'PA',
+    'PB', 'PR', 'PE', 'PI', 'RJ', 'RN','RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
+df['estado_sigla'] = df['estado_sigla'].str.upper().apply(lambda x: x if estados_br else 'Desconhecido')
+
+print('Dados tratados:\n',df.head())
+
+df['cpf'] = df['cpf_mascara']
+df['idade'] = df['idade_ajustada']
+df['endereco'] = df['endereco_curto']
+df['estado'] = df['estado_sigla']
+df_salvar = df[['nome', 'cpf', 'idade', 'data', 'endereco', 'bairro','estado' ]]
+df_salvar.to_csv('Clientes_tratados.csv', index=False)
+
+print('Novo DataFrame: \n', pd.read_csv('Clientes_tratados.csv'))
